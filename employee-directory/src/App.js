7d500c57126c6cf637d40db737c6be7ce67f1855
Tami Shute <tamaralynnrenee@gmail.com>
2020-05-11 11:35:47 -0400

    Search/ Nav Combined

--- employee-directory/src/App.js
+++ employee-directory/src/App.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import './App.css';
 import List from './components/List/list.js';
-import Navbar from './components/Navbar/navbar.js';
-import { ContainedButtons, BasicTextFields } from  './components/Search/search.js';
+import Search from  './components/Search/search.js';
 
 function App() {
   return (
     <div className="App">
-      {/* <Navbar/> */}
-      <BasicTextFields/>
-      <ContainedButtons/>
+      <Search/>
       <List/>
     </div>
   );
